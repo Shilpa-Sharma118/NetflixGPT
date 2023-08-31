512aef1be4081d31a9910fad9682e46e9de8b1d0
tokenadmin <tokenadmin@88665a36ff1b.ant.amazon.com>
2023-08-31 13:57:54 -0700

    configured tailwind CSS

---
+++ netflix-gpt/README.md
@@ -0,0 +1,2 @@
+1. Create create-react-app
+2. Configured TailwindCSS
---
+++ netflix-gpt/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-fuchsia-800">
+      <p>Hello Netflix!!!</p>
+    </div>
+  );
+}
+
+export default App;
